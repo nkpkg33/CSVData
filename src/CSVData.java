cc8ba00f99aa87fcb8579e6ca9f46b2863d739bb
Neha Konakalla <nehakonakalla@Nehas-MacBook.local>
2016-10-12 11:23:53 -0700

    added JavaDoc comments

--- src/CSVData.java
+++ src/CSVData.java
@@ -31,9 +31,9 @@ public class CSVData {
 	}
 	
 	/***
-	 * Re
-	 * @param filename
-	 * @return
+	 * Returns a double array converted from the csv file
+	 * @param filename the file to convert
+	 * @return the converted double array
 	 */
 	
 	public static double[][] convertTo2DArray(String filename){
@@ -42,10 +42,21 @@ public class CSVData {
 		
 	}
 	
+	/***
+	 * Saves changes to the CSVData
+	 * @param filename the file with the new changes
+	 */
+	
 	public static void saveToFile(String filename){
 		
 		
 	}
+	
+	/***
+	 * Returns a double array which is a row of the data
+	 * @param rowIndex the index of the returned row 
+	 * @return the row of index rowIndex
+	 */
 	
 	public static double[] getRow(int rowIndex){
 		
@@ -53,11 +64,23 @@ public class CSVData {
 		
 	}
 	
+	/***
+	 * Returns a double array which is a column of the data
+	 * @param colIndex the index of the returned column
+	 * @return the column of index colIndex
+	 */
+	
 	public static double[] getCol(int colIndex){
 		
 		return null;
 		
 	}
+	
+	/***
+	 * Returns a double array which is a column of the data
+	 * @param colName the name of the returned column
+	 * @return the column of name colName
+	 */
 	
 	public static double[] getCol(String colName){
 		
@@ -65,11 +88,24 @@ public class CSVData {
 		
 	}
 	
+	/***
+	 * Returns a double[][] array which contains multiple rows of data
+	 * @param indices the indices of the returned rows
+	 * @return the array containing rows of input indices
+	 */
+	
 	public static double[][] getRows(int[] indices){
 		
 		return null;
 		
 	}
+
+	/***
+	 * Returns a double[][] array which contains multiple rows of data
+	 * @param startIndex the index of the first row returned
+	 * @param endIndex the index of the last row returned
+	 * @return the array of the range startIndex, endIndex
+	 */
 	
 	public static double[][] getRows(int startIndex, int endIndex){
 		
@@ -77,11 +113,24 @@ public class CSVData {
 		
 	}
 	
+	/***
+	 * Returns a double[][] array which contains multiple columns of data
+	 * @param indices the indices of the returned columns
+	 * @return the array containing the columns of input indices
+	 */
+	
 	public static double[][] getCols(int[] indices){
 		
 		return null;
 		
 	}
+	
+	/***
+	 * Returns a double[][] array which contains multiple columns of data
+	 * @param startIndex the index of the first column returned
+	 * @param endIndex the index of the last column returned
+	 * @returnthe array of the range startIndex, endIndex
+	 */
 	
 	public static double[][] getCols(int startIndex, int endIndex){
 		
@@ -89,31 +138,67 @@ public class CSVData {
 		
 	}
 	
+	/***
+	 * Returns a double[][] array which contains multiple columns of data
+	 * @param colNames the names of the returned columns
+	 * @return the array containing the columns of input names
+	 */
+	
 	public static double[][] getCols(String[] colNames){
 		
 		return null;
 		
 	}
 	
+	/***
+	 * Updates an entry in the data
+	 * @param rowIndex the row index of the entry
+	 * @param colIndex the column index of the entry
+	 * @param newValue the value to update the entry
+	 */
+	
 	public static void setEntry(int rowIndex, int colIndex, double newValue){	
 		
 		
 	}
+	
+	/***
+	 * Updates a row in the data
+	 * @param rowindex the index of the updated row
+	 * @param newValues the values to update the row
+	 */
 	
 	public static void setRow(int rowindex, double[] newValues){	
 		
 		
 	}
 	
+	/***
+	 * Updates a column in the data
+	 * @param colIndex the index of the updated column
+	 * @param newValues the values to update the column
+	 */
+	
 	public static void setCol(int colIndex, double[] newValues){	
 		
 		
 	}
 	
+	/***
+	 * Updates a column in the data
+	 * @param colName the name of the updated column
+	 * @param newValues the values to update the column
+	 */
+	
 	public static void setCol(String colName, double[] newValues){	
 		
 		
 	}
+	
+	/***
+	 * Returns a String array of the column names
+	 * @return the column names
+	 */
 	
 	public static String[] getColumnNames(){
 		
